e145495d2e36a60b81304db42c9fc42425f28246
fjbMultisys <jaybergonia@Jays-MacBook-Pro.local>
2022-04-30 02:07:33 +0800

    initial commit

---
+++ SwiftUIxCombineXnewsAPI/SwiftUIxCombineXnewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIxCombineXnewsAPIApp.swift
+//  SwiftUIxCombineXnewsAPI
+//
+//  Created by Jay Bergonia on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIxCombineXnewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
